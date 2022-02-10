154f7b77eadaf4afb706568103db8d0c35f47c2d
Yoseph <yosephoche@gmail.com>
2022-02-10 18:54:18 +0800

    separate css and js code into another file

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,58 @@
-// selector
+// Selector
+const modal = document.getElementById('modal-rating');
+const starElements = document.querySelectorAll('.c-star');
+const reviewInput = document.getElementById('review');
+const btnSubmit = document.getElementById('submit');
+let rate;
 
-// function
+// Event Listener
+starElements.forEach((star, clickedIndex) => {
+    star.setAttribute("fill", "lightgrey")
+    
+    star.addEventListener("mouseover", () => {
+        rateStar(clickedIndex)
+    });
+
+    star.addEventListener("click", () => {
+        rate = star.starValue;
+    })
+});
+
+btnSubmit.addEventListener("click", () => {
+    const review = reviewInput.value; 
+    // TODO: call API to save review here
+
+    closeModal()
+});
+
+document.addEventListener('click', (event) => {
+    const boolIsOutside = document.getElementById("bg-gray").isSameNode(event.target);
+    if (boolIsOutside) {
+        closeModal()
+    }
+});
+
+
+// Function
+closeModal()
+
+function rateStar(clickedIndex) {
+    star.starValue = clickedIndex+1
+    starElements.forEach((otherStar, otherIndex) => {
+        if (otherIndex <= clickedIndex) {
+            otherStar.setAttribute("fill", "#fed94b")
+        }else{
+            otherStar.setAttribute("fill", "lightgrey")
+        }
+    });
+}
+
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.remove('hidden');
+}
+
+function closeModal() {
+    modal.classList.add('hidden');
+}
+
